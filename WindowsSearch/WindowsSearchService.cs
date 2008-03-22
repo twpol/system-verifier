7208de811699d0ea61c9fc0a71cc580ca2ce8b56
James Ross <twpol@users.noreply.github.com>
2008-03-22 18:26:05 +0000

    More Windows search service stuff - it now scans for IFilters in the 3 normal ways and returns the one in use, although it's not completely accurate yet. Change configuration for both plugins so they dump their release executables into the main application's release folder, like they already do for debug.

--- WindowsSearch/WindowsSearchService.cs
+++ WindowsSearch/WindowsSearchService.cs
@@ -73,13 +73,99 @@ namespace WindowsSearch
 			DefaultScanItem item;
 			current++;
 
-			if (true) {
+			ProcessExtensionFor(extension, rv);
+
+			if (false) {
 				item = new DefaultScanItem(extension);
 				item.Properties["Description"] = item.Type;
 				rv.Add(item);
 			}
 
 			return rv;
+		}
+
+		void ProcessExtensionFor(string extension, List<IScanItem> rv) {
+			DefaultScanItem item;
+
+			{
+				string persistHandler = GetPersistentHandler(extension);
+				string iFilterClass = GetIFilterFromPersistentHandler(persistHandler);
+				string library = GetDefaultValueFromKey(@"SOFTWARE\Classes\CLSID\" + iFilterClass + @"\InProcServer32");
+				if (iFilterClass != "") {
+					item = new DefaultScanItem(extension);
+					item.Properties["Description"] = "PH: " + library + " / " + GetClassIDName(iFilterClass) + " / " + persistHandler;
+					rv.Add(item);
+					return;
+				}
+			}
+			{
+				string contentType = GetValueFromKey(@"SOFTWARE\Classes\" + extension, "Content Type");
+				string classID = GetValueFromKey(@"SOFTWARE\Classes\MIME\Database\Content Type\" + contentType, "CLSID");
+				string persistHandler = GetPersistentHandler(classID);
+				string iFilterClass = GetIFilterFromPersistentHandler(persistHandler);
+				string library = GetDefaultValueFromKey(@"SOFTWARE\Classes\CLSID\" + iFilterClass + @"\InProcServer32");
+				if (iFilterClass != "") {
+					item = new DefaultScanItem(extension);
+					item.Properties["Description"] = "CT: " + library + " / " + GetClassIDName(iFilterClass) + " / " + GetClassIDName(persistHandler) + " / " + GetClassIDName(classID) + " / " + contentType;
+					rv.Add(item);
+					return;
+				}
+			}
+			{
+				string handlerName = GetDefaultValueFromKey(@"SOFTWARE\Classes\" + extension);
+				string classID = GetDefaultValueFromKey(@"SOFTWARE\Classes\" + handlerName + @"\CLSID");
+				string persistHandler = GetPersistentHandler(classID);
+				string iFilterClass = GetIFilterFromPersistentHandler(persistHandler);
+				string library = GetDefaultValueFromKey(@"SOFTWARE\Classes\CLSID\" + iFilterClass + @"\InProcServer32");
+				if (iFilterClass != "") {
+					item = new DefaultScanItem(extension);
+					item.Properties["Description"] = "DT: " + library + " / " + GetClassIDName(iFilterClass) + " / " + GetClassIDName(persistHandler) + " / " + GetClassIDName(classID) + " / " + handlerName;
+					rv.Add(item);
+					return;
+				}
+			}
+		}
+
+		string GetDefaultValueFromKey(string keyName) {
+			RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(keyName);
+			if (key == null) {
+				return "";
+			}
+			using (key) {
+				return "" + key.GetValue("");
+			}
+		}
+
+		string GetValueFromKey(string keyName, string valueName) {
+			RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(keyName);
+			if (key == null) {
+				return "";
+			}
+			using (key) {
+				return "" + key.GetValue(valueName);
+			}
+		}
+
+		string GetClassIDName(string classID) {
+			string name = GetDefaultValueFromKey(@"SOFTWARE\Classes\CLSID\" + classID);
+			if (name == "") {
+				return classID;
+			}
+			return name;
+		}
+
+		string GetPersistentHandler(string extensionOrClassID) {
+			if (extensionOrClassID.StartsWith(".")) {
+				return GetDefaultValueFromKey(@"SOFTWARE\Classes\" + extensionOrClassID + @"\PersistentHandler").ToUpper();
+			}
+			if (extensionOrClassID.StartsWith("{")) {
+				return GetDefaultValueFromKey(@"SOFTWARE\Classes\CLSID\" + extensionOrClassID + @"\PersistentHandler").ToUpper();
+			}
+			return "";
+		}
+
+		string GetIFilterFromPersistentHandler(string classID) {
+			return GetDefaultValueFromKey(@"SOFTWARE\Classes\CLSID\" + classID + @"\PersistentAddinsRegistered\{89BCB740-6119-101A-BCB7-00DD010655AF}").ToUpper();
 		}
 
 		public void PostProcess() {
